55fcc31b9950012b198b0515f945517894943968
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-02-25 17:05:00 +0100

    code  update after help

--- js/app.js
+++ js/app.js
@@ -1,28 +1,33 @@
-// Import the entire three.js core library.
-import * as THREE from "three";
+// // // Import the entire three.js core library.
+// // import * as THREE from "three";
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
 
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-//scene
 const scene = new THREE.Scene();
-console.log(scene);
-
-// camera
 const camera = new THREE.PerspectiveCamera(
-  45,
+  75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-// object for browser
-const geometry = new THREE.SphereGeometry(15, 32, 16);
-const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// render
-const render = new OrbitControls(camera, renderer.domElement);
+const geometry = new THREE.BoxGeometry(2, 1, 4);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0099 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.02;
+  cube.rotation.y += 0.0;
+
+  renderer.render(scene, camera);
+}
+
+animate();
