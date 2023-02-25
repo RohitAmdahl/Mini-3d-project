676403e55a78b2e9d71125b5b7f39f391fc7de5f
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-02-25 16:13:41 +0100

    update

--- js/app.js
+++ js/app.js
@@ -5,8 +5,11 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+//scene
 const scene = new THREE.Scene();
+console.log(scene);
 
+// camera
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -14,6 +17,11 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const axesHelper = new THREE.AxesHelper(5);
+// object for browser
+const geometry = new THREE.SphereGeometry(15, 32, 16);
+const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
+// render
 const render = new OrbitControls(camera, renderer.domElement);
