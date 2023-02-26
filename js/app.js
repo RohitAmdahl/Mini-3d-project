962d5fce1d9e6baca67cf3034c18946704ce6423
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-02-26 23:37:24 +0100

    updated with grids and plane

--- js/app.js
+++ js/app.js
@@ -4,9 +4,11 @@ import * as THREE from "https://unpkg.com/three/build/three.module.js";
 
 //-------------------importing from node modules but getting type error
 // import * as THREE from "../node_modules/three/build/three.js";
+
 const scene = new THREE.Scene();
 //--------------------------------------orbit control trying to get it but getting error type error
-// import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
+//import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
+//import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 //--------------------------------------orbit control trying to get it but getting error
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -15,27 +17,26 @@ document.body.appendChild(renderer.domElement);
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
-  0.1,
+  1,
   1000
 );
-//-----------------------------------------------------orbit control
-// const orbit = new OrbitControls(camera, renderer, domElement);
-// orbit.update();
 
-//-----------------------------------------------------
 camera.position.set(0, 2, 5);
 // axes helper
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 //-----------------------------------------------------
+
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const boxMaterial = new THREE.MeshBasicMaterial({ color: "#0085FF" });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
+
 //-create plane grids----------------
-const geometry = new THREE.PlaneGeometry(5, 5);
+const geometry = new THREE.PlaneGeometry(8, 5);
 const material = new THREE.MeshBasicMaterial({
   color: "#373737",
+  side: THREE.DoubleSide,
 });
 const plane = new THREE.Mesh(geometry, material);
 scene.add(plane);
@@ -48,7 +49,12 @@ scene.add(gridHelper);
 plane.rotation.x = -0.5 * Math.PI;
 // const gridsHelper = new THREE.GridsHelper(35);
 // scene.add(gridsHelper);
+// //-----------------------------------------------------orbit control
 
+// const orbit = new OrbitControls(camera, renderer, domElement);
+// orbit.update();
+
+// //-----------------------------------------------------
 function animate() {
   requestAnimationFrame(animate);
 
@@ -59,4 +65,4 @@ function animate() {
 }
 
 animate();
-renderer.render(scene, camera);
+// renderer.render(scene, camera);
